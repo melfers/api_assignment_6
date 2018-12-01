7d74328fc974682f77652441ce30e6925e8f6a4f
Molly Elfers <elfers.molly@gmail.com>
2018-12-01 10:31:25 -0800

    added listener to form submission

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        const searchTerm = $('#js-search-term').val();
+        const maxResults = $('#js-max-results').val();
+    });
+}
+
+$(watchForm);
