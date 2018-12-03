dae2766eade2224dd66f5823bfd170283e4e6336
Molly Elfers <elfers.molly@gmail.com>
2018-12-03 11:44:58 -0800

    new

--- script.js
+++ script.js
@@ -10,21 +10,25 @@ function formatQueryParams(params) {
     return queryItems.join('&');
   }
 
-function displayResults(responseJson) {
-  // if there are previous results, remove them
-  console.log(responseJson);
-  $('#results-list').empty();
-  // iterate through the items array
-  for (let i = 0; i < responseJson.length; i++){
-    $('#results-list').append(
-      `<li><h3>${responseJson[i].fullName}</h3>
-      <p>${responseJson[i].description}</p>
-      <img src='${responseJson[i].url}'>
-      </li>`
-    )};
-  //display the results section  
-  $('#results').removeClass('hidden');
-}
+  function displayResults(responseJson) {
+    // if there are previous results, remove them
+    console.log(responseJson);
+    $('#results-list').empty();
+    // iterate through the items array
+    for (let i = 0; i < responseJson.length; i++){
+      // for each video object in the items 
+      //array, add a list item to the results 
+      //list with the video title, description,
+      //and thumbnail
+      $('#results-list').append(
+        `<li><h3>${responseJson[i].fullname}</h3>
+        <p>${responseJson[i].description}</p>
+        <img src='${responseJson[i].url}'>
+        </li>`
+      )};
+    //display the results section  
+    $('#results').removeClass('hidden');
+  };
 
 function getParks(query, maxResults=10) {
     const params = {
