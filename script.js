bf68f0552db3a76de73ef49dddac9eb18e0f19ac
Molly Elfers <elfers.molly@gmail.com>
2018-12-03 11:14:15 -0800

    added fetch functions to script.js

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
 
 
+const apiKey = 'yrvZzbilpP8UzCcqQEgUZC3fxE74YrL5YvBtg0nf'; 
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
+
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+  }
+
+function getParks(query, maxResults=10) {
+    const params = {
+      key: apiKey,
+      q: query,
+      maxResults
+    };
+    const queryString = formatQueryParams(params)
+    const url = searchURL + '?' + queryString;
+  
+    console.log(url);
+  
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error(response.statusText);
+        console.log(response.statusText);
+      })
+      .then(responseJson => displayResults(responseJson))
+      .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        console.log(err.message);
+      });
+}
 
 function watchForm() {
     $('form').submit(event => {
         event.preventDefault();
         const searchTerm = $('#js-search-term').val();
         const maxResults = $('#js-max-results').val();
+        getParks(searchTerm, maxResults);
     });
 }
 
